0b357d8ddc1493567cc290c5ea7c68eaf271aab1
JuanMercado <mercadojuanc25@gmail.com>
2023-05-27 02:52:25 -0300

    Se crea la funcion cargarPelicula

--- main.c
+++ main.c
@@ -23,3 +23,25 @@ void menu() {
     printf("3. Listar películas\n");
     printf("4. Salir\n");
 }
+
+void cargarPelicula(struct pelicula* p) {
+
+    printf("\n\n");
+    printf("   *** Carga de pelicula ***");
+    printf("\n");
+
+    printf("Ingrese el titulo: ");
+    scanf(" %[^\n]s", p->titulo);
+
+    printf("Ingrese el genero: ");
+    scanf(" %[^\n]s", p->genero);
+
+    printf("Ingrese la duracion (en minutos): ");
+    scanf("%d", &p->duracion);
+
+    printf("Ingrese el anio de la pelicula: ");
+    scanf("%d", &p->anio);
+    
+    printf("Ingrese el nombre del director: ");
+    scanf(" %[^\n]s", p->director);
+}
